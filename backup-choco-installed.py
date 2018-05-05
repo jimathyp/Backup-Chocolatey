39e5fd99b73e0b18c9f9762b2c15af6c4b810d61
James Pettengell <>
2018-05-05 15:08:12 +1200

    Added jputils, using timestamp to version output command

--- backup-choco-installed.py
+++ backup-choco-installed.py
@@ -9,17 +9,21 @@ output_list = "choco-installed.txt"
 import subprocess
 import os
 import re
+import datetime
+
+import jputils
+
+
 
 
 output_file = "choco-installed.txt"
 output_file = os.path.join(os.getcwd(), output_file)
 print(output_file)
 
-print("running choco list")
-subprocess.call(["choco", "list", "-lo", ">", output_file], shell=True)
+# print("running choco list")
+# subprocess.call(["choco", "list", "-lo", ">", output_file], shell=True)
 
-cmd_script = "choco-reinstall.ps1" 
-
+cmd_script = "choco-reinstall-{}.ps1".format(jputils.datetime_timestamp()) 
 print("parsing file")
 #read the output_file in
 with open(output_file, "r") as infile, open(cmd_script, "w") as outfile:
@@ -33,10 +37,13 @@ with open(output_file, "r") as infile, open(cmd_script, "w") as outfile:
             # print(search)
             # print(search.group(0))
             # print(search.groups())
-            print(search.group(1))
+            # print(search.group(1))
             cmd.append(search.group(1))
     # cmd = " ".join(cmd, search.group(1))
     print(cmd)
+    outfile.write(" ".join(cmd))
+    
+    
             # for 
     # remove 1st line "Chocolatey v0.10.8"
     # remove version from each line 7zip.commandline 16.02.0.20170209
