f0f4cdd42ed9c6bb152bc7d66ff48bbc190b026b
James Pettengell <>
2018-05-05 14:38:24 +1200

    Used regex to match package (excl line with Chocolatey version), but regex is greedy

--- backup-choco-installed.py
+++ backup-choco-installed.py
@@ -15,18 +15,22 @@ output_file = "choco-installed.txt"
 output_file = os.path.join(os.getcwd(), output_file)
 print(output_file)
 
+print("running choco list")
 subprocess.call(["choco", "list", "-lo", ">", output_file], shell=True)
 
 cmd_script = "choco-reinstall.ps1" 
 
+print("parsing file")
 #read the output_file in
 with open(output_file, "r") as f:
 
 # parse file:
     for line in f:
         # if not line.startswith("Chocolatey") and not line[0].isdigit():
-
-        print(line)
+        search = re.search("^(?!Chocolatey.*)(.*)\s\d.*", line)
+        if search:
+            print(search)
+            # print(search.group(0))
     # remove 1st line "Chocolatey v0.10.8"
     # remove version from each line 7zip.commandline 16.02.0.20170209
     # remove last line 70 packages installed.
